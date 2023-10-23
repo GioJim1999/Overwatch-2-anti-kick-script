4fe9ec3a9806851c617887fd0e49cc45543fa3c2
Gio Jimenez <132255229+GioJim1999@users.noreply.github.com>
2023-10-22 22:11:45 -0700

    Add files via upload

---
+++ autoMove.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+import time as t
+
+pg.FAILSAFE = True
+
+t.sleep(3)
+while(True):
+        pg.keyDown('w')
+        t.sleep(0.5)
+        pg.keyUp('w')
+        pg.keyDown('s')
+        t.sleep(0,5)
+        pg.keyUp('s')
